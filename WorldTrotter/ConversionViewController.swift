ce7e788c41b2511ab361172e11cd2f4ec71222b0
Kaleab <kaleabsolomon470@gmail.com>
2019-07-17 11:26:52 -0700

    Question 1 Done

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -4,7 +4,7 @@
 
 import UIKit
     // TODO: Mark the ViewController as conforming to the UITextFieldDelegate Protocol
-class ConversionViewController: UIViewController{ //, UITextFieldDelegate {
+class ConversionViewController: UIViewController, UITextFieldDelegate{ //, UITextFieldDelegate {
     
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
@@ -22,9 +22,20 @@ class ConversionViewController: UIViewController{ //, UITextFieldDelegate {
     // TODO: Modify code to reject (return false) if it finds any letters in the replacement string
     //  (hint-use Documentation to find a NSCharacterSet collection for letters, and a String method that finds a range using a NSCharacterSet)
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        //MY CODE STARTS HERE
+        let lowercaseLetters = CharacterSet.lowercaseLetters
+        
+        if (string.lowercased().rangeOfCharacter(from: lowercaseLetters) != nil){
+            return false;
+        }
+        
+        //ENDS HERE
         
         let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
         let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        
         
         if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
             return false
@@ -36,6 +47,16 @@ class ConversionViewController: UIViewController{ //, UITextFieldDelegate {
     // TODO: Add and modify the method to build expectation for the output by changing the celsiusLabel when the input field is selected
     // modify the celsiusLabel text to be a single question mark
     // modify the celsiusLabel color to be 60% red, 60% green, and 40% blue (refer to the Developer Documentation for UIColor)
+    
+    
+    //FIXME -- MY CODE DOES STARTS HERE
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        if textField == self.textField{
+            self.textField.text = ""
+            celsiusLabel.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.4, alpha: 1);
+            
+        }
+    }
 
     
     // EVENT HANDLER METHOD : Called when TextField is Changed (notice the optional binding)
@@ -45,6 +66,7 @@ class ConversionViewController: UIViewController{ //, UITextFieldDelegate {
         } else {
             fahrenheitValue = nil
         }
+
     }
     //Stored Properties for Fahrenheit Temperature Measurement w/Observer
     var fahrenheitValue: Measurement<UnitTemperature>? {
@@ -65,7 +87,7 @@ class ConversionViewController: UIViewController{ //, UITextFieldDelegate {
         if let celsiusValue = celsiusValue {
             celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         } else {
-            celsiusLabel.text = "???"
+            celsiusLabel.text = "?"
         }
     }
     // Limits the number of decimal places in the output label to 1
